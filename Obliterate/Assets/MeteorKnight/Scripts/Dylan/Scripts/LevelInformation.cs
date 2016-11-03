29ca10bdddb6d17cdabbe56d3368c67d31d1d4b3
Syharka <thomsonsst@live.com>
2016-11-04 07:45:36 +1300

    MainMenu-VFX-Update

---
+++ Obliterate/Assets/MeteorKnight/Scripts/Dylan/Scripts/LevelInformation.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelInformation : MonoBehaviour {
+    public int Level = -1;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
